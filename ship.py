d1a2bfc434f990fe4b7a39764a75cf5a79e1e0a6
admin <123456789@136.com>
2024-04-14 22:24:42 +0800

    Ship move

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load("images/ship.bmp")
@@ -14,9 +15,25 @@ class Ship:
         self.scaled_height = 50
         self.scaledimage = pygame.transform.scale(self.image, (self.scaled_width, self.scaled_height))
         self.rect = self.scaledimage.get_rect()
+        # 在飞船的属性x中存储一个浮点数
+        self.x = float(self.rect.x)
 
         # 每膄新飞船都放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        # 更新飞船的属性x的值，而不是其外接矩形的属性x的值
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # 根据self.x更新rect对象的值
+        self.rect.x = self.x
 
     def blitme(self):
         """在指定位置绘制飞船"""
