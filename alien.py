57f540e01f21d1bef5db9f7700e405324db5ff6f
admin <123456789@136.com>
2024-04-15 21:58:48 +0800

    Init alien

--- alien.py
+++ alien.py
@@ -10,7 +10,7 @@ class Alien(Sprite):
         self.screen = ai_game.screen
 
         # 加载外星人图像并设置其rect属性
-        self.image = pygame.image.load()
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         # 每个外星人最初都在屏幕的左上角附近
